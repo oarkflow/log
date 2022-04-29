7624ba05f5fa5773669bfa83b7b9df24087b750b
Sujit <itsursujit@gmail.com>
2022-04-29 11:58:27 +0545

    Add gelf and update dependencies

--- logger.go
+++ logger.go
@@ -1976,8 +1976,6 @@ func wlprintf(w Writer, level Level, format string, args ...interface{}) (int, e
 func b2s(b []byte) string { return *(*string)(unsafe.Pointer(&b)) }
 
 //go:noescape
-//go:linkname now time.now
-func now() (sec int64, nsec int32, mono int64)
 
 //go:noescape
 //go:linkname absDate time.absDate
